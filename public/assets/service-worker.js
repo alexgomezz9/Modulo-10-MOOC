ea3a9bd932a89bff9475dd3f332dfa647b45efc4
Alexgomezz9 <Ale533agr@gmail.com>
2024-07-13 19:41:55 +0200

    Initial commit

---
+++ public/assets/service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'shooter-game-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/game.css',
+  '/main.js',
+  '/assets/character.png',  // y otros archivos estáticos necesarios
+];
+
+// Instalar el Service Worker y cachear todos los recursos necesarios
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar solicitudes y servir recursos cacheados
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Actualizar el Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
